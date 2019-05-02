be64b1472855e60c859a28042adb6c04ecab588a
YanivBohbot <48159579+YanivBohbot@users.noreply.github.com>
2019-05-02 16:45:49 +0300

    Add files via upload

---
+++ main_avoda3.cpp
@@ -0,0 +1,14 @@
+/*Full Name:Yaniv Bohbot
+ID :204892749
+Campus :Ashdod
+*/
+#define _CRT_SECURE_NO_WARNINGS
+#include "College.h"
+
+
+int main()
+{
+	College college;
+	college.menu();
+	return 0;
+}
